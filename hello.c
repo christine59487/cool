b8e3df42aa080753c3541712290651ced988a25d
christine54 <christine5487@gmail.com>
2021-05-16 02:02:34 +0800

    test

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+  int a,b;
+  printf("hey, type sth, i'll show you how to do math, idiot head\n");
+  scanf("%d %d",&a,&b);
+  printf("a+b equals to: %d\n",a+b);
+
+  return 0;
+}
